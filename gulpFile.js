276249e3ccdf6c1bb894628fb5b10eb051edcf99
chenthooran <kchenthoor@hotmail.com>
2016-07-07 13:46:14 +0530

    Cache busting strategy included.

---
+++ gulpFile.js
@@ -0,0 +1,16 @@
+﻿// Include gulp
+var gulp = require('gulp');
+
+var cacheBuster = require('gulp-cache-bust');
+
+// cacheBuster looks at the css and js files and appends a hash to the
+// request to cause the file to get reloaded when the file changes.
+gulp.task('cacheBuster', [], function () {
+    return gulp.src('index.html')
+        .pipe(cacheBuster())
+        .pipe(gulp.dest('.'));
+});
+
+
+
+gulp.task('default', ['cacheBuster']);
