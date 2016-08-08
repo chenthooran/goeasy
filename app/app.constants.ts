976e0996aabe48eb88fadd55728f995f343cc7df
chenthooran <kchenthoor@hotmail.com>
2016-08-08 12:25:53 +0530

    app.constant changes

--- app/app.constants.ts
+++ app/app.constants.ts
@@ -3,7 +3,7 @@
 @Injectable()
 export class Configuration {
     //public Server: string = "http://localhost:54736/";
-	public Server: string = "http://yaycollaborationapi.azurewebsites.net/";
+public Server: string = "http://yaycollaborationapi.azurewebsites.net/";
 	//public Server: string = "http://yayprodapi.azurewebsites.net/";
     public ApiUrl: string = "api/";
     public ServerWithApiUrl = this.Server + this.ApiUrl;
